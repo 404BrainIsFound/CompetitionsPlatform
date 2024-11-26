13c54ebbc3c2d598d83b4fe36386b3da5e0edef0
Jonathan Mohammed <jonathan.mohammed.15@gmail.com>
2024-11-26 00:55:43 +0000

    Updated Score Manager to Observer Interface Only, Update Competition Team to Concrete Subject

--- App/models/competition_team.py
+++ App/models/competition_team.py
@@ -1,7 +1,7 @@
 from App.database import db
 from .score_manager import *
 
-class CompetitionTeam(db.Model, Subject):
+class CompetitionTeam(db.Model):
     __tablename__ = 'competition_team'
     
     id = db.Column(db.Integer, primary_key=True)
@@ -9,13 +9,25 @@ class CompetitionTeam(db.Model, Subject):
     team_id =  db.Column(db.Integer, db.ForeignKey('team.id'), nullable=False)
     points_earned = db.Column(db.Float, default=0)
     rating_score = db.Column(db.Float, default=0)
+    managers = []
 
     def __init__(self, comp_id, team_id):
-      super().__init__()
       self.comp_id = comp_id
       self.team_id = team_id
       self.points_earned = 0
       self.rating_score = 0
+      self.managers = []
+    
+    def attach(self, manager: ScoreManager) -> None:
+        if manager not in self.managers:
+            self.managers.append(manager)
+    
+    def detach(self, manager: ScoreManager) -> None:
+        self.managers.remove(manager)
+
+    def notify(self, message):
+        for manager in self.managers:
+            manager.update(message)
 
     def update_points(self, points_earned):
       self.points_earned = points_earned
--- App/models/score_manager.py
+++ App/models/score_manager.py
@@ -1,23 +1,5 @@
-# Observer Design Pattern
+# Observer
 
 class ScoreManager:
     def update(self, message) -> None:
         pass
-
-
-class Subject:
-    managers = []
-
-    def __init__(self):
-        self.managers = []
-
-    def attach(self, manager: ScoreManager) -> None:
-        if manager not in self.managers:
-            self.managers.append(manager)
-
-    def detach(self, manager: ScoreManager) -> None:
-        self.managers.remove(manager)
-
-    def notify(self, message):
-        for manager in self.managers:
-            manager.update(message)
