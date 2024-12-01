ff5fec3d00101b7635d305d72ced3055258bbcaa
General-Pete <alejandro.marin@my.uwi.edu>
2024-12-01 20:44:27 +0000

    attempting to make a graph, initial commit - ignore

--- App/static/main.js
+++ App/static/main.js
@@ -27,8 +27,70 @@ function closeNav(){
     document.getElementById("sidepanel").style.width = "0";
 }
 
-/*function closeMessage(){
-    document.getElementById("error_message").style.display = "none";
-}*/
+
+//Below is added code - not in original JS
+
+const modal = document.getElementById("statsModal");
+const rankingsTableBody = document.querySelector("#rankingsTable tbody");
+
+function openModal() {
+    modal.style.display = "block";
+
+    const username = document.querySelector(".profile-details h2").textContent.trim();
+    const history = fetch(`/students/${username}/rankings`);
+
+    //create a graph below:
+
+    const xValues = [50,60,70,80,90,100,110,120,130,140,150];
+    const yValues = [7,8,8,9,9,9,10,11,14,14,15];
+
+    new Chart("myChart", {
+        type: "line",
+        data: {
+          labels: xValues,
+          datasets: [{
+            fill: false,
+            lineTension: 0,
+            backgroundColor: "rgba(0,0,255,1.0)",
+            borderColor: "rgba(0,0,255,0.1)",
+            data: yValues
+          }]
+        },
+        options: {
+          legend: {display: false},
+          scales: {
+            yAxes: [{ticks: {min: 6, max:16}}],
+          }
+        }
+      });
+
+    // const username = document.querySelector(".profile-details h2").textContent.trim();
+    // fetch(`/students/${username}/rankings`)
+    //     .then(response => {
+    //         if (!response.ok) {
+    //             throw new Error("Failed to fetch rankings for }" + username);
+    //         }
+    //         return response.json();
+    //     })
+    //     .then(data => {
+    //         populateRankingsTable(data);
+    //     })
+    //     .catch(error => {
+    //         console.error("Error fetching rankings:", error);
+    //         rankingsTableBody.innerHTML = `<tr><td colspan="2">Failed to load data for {username}</td></tr>`;
+    //     });
+}
+
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function (event) {
+    if (event.target === modal) {
+        closeModal();
+    }
+};
 
 main();
